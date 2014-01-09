7e9b26ce52a5aeeb279737837970c4510acc9331
alberto <ciopper90dev@gmail.com>
2014-01-09 18:53:10 +0100

    piccole incompatibilita con android 4 (funzionava su 2.3 ma non su 4 o successivi) ora dovrebbe essere compatibile sia con 2.3 che con 4

--- src/it/gaiacri/mobile/Accesso.java
+++ src/it/gaiacri/mobile/Accesso.java
@@ -16,7 +16,6 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -26,6 +25,7 @@ import android.widget.Toast;
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
+@SuppressLint("SetJavaScriptEnabled")
 public class Accesso extends Activity {
 
 	/**
@@ -38,65 +38,14 @@ public class Accesso extends Activity {
 	public SharedPreferences sharedPref;
 
 
-	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	    requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-	    requestWindowFeature(Window.FEATURE_PROGRESS);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		requestWindowFeature(Window.FEATURE_PROGRESS);
 		sharedPref=this.getSharedPreferences("LOGIN", Context.MODE_PRIVATE);
 		//setContentView(R.layout.activity_accesso);
-		context = this.getApplicationContext();
-		if(webview == null){
-			Log.i("create","webview");
-			webview = new WebView(this);
-			webview.getSettings().setJavaScriptEnabled(true);
-
-			final Activity activity = this;
-			webview.setWebChromeClient(new WebChromeClient() {
-				public void onProgressChanged(WebView view, int progress) {
-					setProgress(progress * 100);
-		              if(progress == 100) {
-		                 setProgressBarIndeterminateVisibility(false);
-		                 setProgressBarVisibility(false);
-		              }//activity.setProgress(progress * 1000);
-				}
-			});
-			webview.setWebViewClient(new WebViewClient() {
-				public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-					Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
-				}
-				public void onPageFinished(WebView view, String url) {
-					HashMap<String, String> data = new HashMap<String, String>();
-					RichiestaWelcome hello=new RichiestaWelcome(data);
-					hello.execute();
-					
-					/*if(url_test==null){
-						Log.d("1", "1");
-						url_test=webview.getUrl();
-					}else{
-						Log.d("2", "2");
-						if(!url_test.equals(webview.getUrl())){
-							url_test=webview.getUrl();
-							Log.d("3", "3");
-							Intent i= new Intent(Accesso.this,MenuPrincipale.class);
-							i.putExtra("sid", sid);		   
-							startActivity(i);
-							webview=null;
-							finish();
-						}
-					}*/
-					
-					
-				}
-			});
-			HashMap<String, String> data = new HashMap<String, String>();
-			login = new RichiestaLogin(data);
-			login.execute();
-			setProgressBarIndeterminateVisibility(true);
-			setProgressBarVisibility(true);
-		}
-		setContentView(webview);
+		loadWebView();
 		// Let's display the progress in the activity title bar, like the
 		// browser app does.
 		//getWindow().requestFeature(Window.FEATURE_PROGRESS);
@@ -113,11 +62,92 @@ public class Accesso extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onPause() {
+		Log.d("prova","pausa");
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		Log.d("prova","stop");
+		if(webview!=null){
+			webview.clearView();
+			webview=null;
+		}
+		super.onStop();
+	}
+
+	@Override
+	protected void onRestart() {
+		Log.d("prova","restart");
+		loadWebView();
+		super.onRestart();
+	}
+
+
+
+	private void loadWebView() {
+		context = this.getApplicationContext();
+		if(webview == null){
+			Log.i("create","webview");
+			webview = new WebView(this);
+			webview.getSettings().setJavaScriptEnabled(true);
+
+			final Activity activity = this;
+			webview.setWebChromeClient(new WebChromeClient() {
+				public void onProgressChanged(WebView view, int progress) {
+					setProgress(progress * 100);
+					if(progress == 100) {
+						setProgressBarIndeterminateVisibility(false);
+						setProgressBarVisibility(false);
+					}//activity.setProgress(progress * 1000);
+				}
+			});
+			webview.setWebViewClient(new WebViewClient() {
+				public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+					Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+				}
+				public void onPageFinished(WebView view, String url) {
+					HashMap<String, String> data = new HashMap<String, String>();
+					RichiestaWelcome hello=new RichiestaWelcome(data);
+					hello.execute();
+
+					/*if(url_test==null){
+						Log.d("1", "1");
+						url_test=webview.getUrl();
+					}else{
+						Log.d("2", "2");
+						if(!url_test.equals(webview.getUrl())){
+							url_test=webview.getUrl();
+							Log.d("3", "3");
+							Intent i= new Intent(Accesso.this,MenuPrincipale.class);
+							i.putExtra("sid", sid);		   
+							startActivity(i);
+							webview=null;
+							finish();
+						}
+					}*/
+
+
+				}
+			});
+			HashMap<String, String> data = new HashMap<String, String>();
+			login = new RichiestaLogin(data);
+			login.execute();
+			setProgressBarIndeterminateVisibility(true);
+			setProgressBarVisibility(true);
+		}
+		setContentView(webview);
+
+	}
+
 
 
 
 	@Override
 	protected void onDestroy() {
+		Log.d("prova","quando passo qui");
 		if(webview!=null){
 			ViewGroup parentViewGroup = (ViewGroup) webview.getParent();
 			if (parentViewGroup != null) {
@@ -140,6 +170,10 @@ public class Accesso extends Activity {
 			if(ris.equals("Errore Internet"))
 				Toast.makeText(context, R.string.error_internet,Toast.LENGTH_LONG).show();
 			else{
+				if(risposta==null){
+					//TODO
+					finish();
+				}
 				//TODO elabora risposta
 				//TODO aggiorna tabella turni della view
 				String url_login="";
@@ -155,7 +189,7 @@ public class Accesso extends Activity {
 
 		}
 	}
-	
+
 	public class RichiestaWelcome extends Richiesta {
 
 		public String metodo() { return "ciao"; }
@@ -167,55 +201,45 @@ public class Accesso extends Activity {
 
 		@Override
 		protected void onPostExecute(String str) {
-				if(str.equals("Errore Internet")){
-					Toast.makeText(context, R.string.error_internet,Toast.LENGTH_LONG).show();
-				}else{
-					SharedPreferences.Editor editor = sharedPref.edit();
-					editor.putString("sid", getSid());
-					editor.commit();
+			if(str.equals("Errore Internet")){
+				Toast.makeText(context, R.string.error_internet,Toast.LENGTH_LONG).show();
+			}else{
+				SharedPreferences.Editor editor = sharedPref.edit();
+				editor.putString("sid", getSid());
+				editor.commit();
 
-					if ( utente != null ) {
-						Log.e("Login", "Gia Identificato");
-						Intent i= new Intent(Accesso.this,MenuPrincipale.class);
-						i.putExtra("sid", sid);		   
-						startActivity(i);
-						webview=null;
-						Accesso.this.finish();
-					}
+				if ( utente != null ) {
+					Log.e("Login", "Gia Identificato");
+					Intent i= new Intent(Accesso.this,MenuPrincipale.class);
+					i.putExtra("sid", sid);		   
+					startActivity(i);
+					webview=null;
+					Accesso.this.finish();
 				}
 			}
+		}
 
 	}
-	
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_HOME || keyCode == KeyEvent.KEYCODE_SEARCH) {
-        	Log.i("down","home");
-        	Log.i("down","search");
-        	//webview=null;
-        	webview=null;
-        	finish();
-        	return true;
-        }
-        if(keyCode == KeyEvent.KEYCODE_BACK){
-        	Log.i("down","back");
-        	if(webview.canGoBack() == true){
-                webview.goBack();
-                return true;
-            }else{
-            	webview=null;
-            }
-        	
-        }
-         return super.onKeyDown(keyCode, event);
-    }
-    
 
-    @Override
-    public void onAttachedToWindow()
-    { 
-        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
-        super.onAttachedToWindow(); 
-    }
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ( keyCode == KeyEvent.KEYCODE_SEARCH) {
+			Log.i("down","search");
+			webview=null;
+			finish();
+			return true;
+		}
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+			Log.i("down","back");
+			if(webview.canGoBack() == true){
+				webview.goBack();
+				return true;
+			}else{
+				webview=null;
+			}
+
+		}
+		return super.onKeyDown(keyCode, event);
+	}
 
 }
