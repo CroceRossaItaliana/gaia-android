5b02efd4765fb47fb9dc47f15d2ddea408089f22
ciopper90 <ciopper90dev@gmail.com>
2014-04-10 18:03:51 +0200

    -sistemata pagina di about #54 -aggiunto apk v 0.6.11

--- src/it/gaiacri/mobile/AboutAttivita.java
+++ src/it/gaiacri/mobile/AboutAttivita.java
@@ -1,95 +1,90 @@
 package it.gaiacri.mobile;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.content.Context;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.SimpleAdapter;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AboutAttivita extends Fragment {
 
-	private Context context;
-	private ListView listview;
-	private ArrayList<String> info,url;
-	
-	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		RelativeLayout v= (RelativeLayout)inflater.inflate(R.layout.activity_about, container, false);
-		context=super.getActivity().getApplicationContext();
-		Log.d("screen", super.getActivity().getRequestedOrientation() +"");		
-		context=super.getActivity();
-		
-		info = new ArrayList<String>();
-		url = new ArrayList<String>();
-		
-		//informazioni su gaia
-		info.add("Versione GAIA Mobile");
-		url.add("v "+getString(R.string.app_version));
-				
-		//link gaiaweb
-		info.add("Sito Web");
-		url.add("https://gaia.cri.it");
-				
-		//informazioni su gaia
-		info.add("Informazioni GAIA");
-		url.add("https://gaia.cri.it/?p=public.about");
-		
-		//link github di gaia Android
-		info.add("Codice Sorgente Gaia Android");
-		url.add("https://github.com/CroceRossaItaliana/gaia-android");
-		
-		//Informazioni Gestione Dati
-		info.add("Informazioni Gestione Dati");
-		url.add("https://gaia.cri.it/?p=public.privacy");
-		
-		listview =(ListView)v.findViewById(R.id.listInfo);//).setText(Html.fromHtml(getString(R.string.prova)));
-		
-		aggiornalist();
-		
+		LinearLayout v= (LinearLayout)inflater.inflate(R.layout.activity_about, container, false);
+
+		((TextView)v.findViewById(R.id.about_title)).setText("Gaia Mobile v "+getString(R.string.app_version));
+
+		//licenza
+		((TextView)v.findViewById(R.id.about_licenza)).setText(Html.fromHtml("Questa applicazione è rilasciata con licenza <a href=\"\">GPL v3</a>"));
+		((TextView)v.findViewById(R.id.about_licenza)).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(AboutAttivita.this.getActivity()); 
+				WebView wv = new WebView(AboutAttivita.this.getActivity());
+				wv.loadUrl("https://raw.githubusercontent.com/CroceRossaItaliana/gaia-android/master/LICENSE.txt");
+				wv.setWebViewClient(new WebViewClient() {
+					@Override
+					public boolean shouldOverrideUrlLoading(WebView view, String url) {
+						view.loadUrl(url);
+
+						return true;
+					}
+				});
+				alert.setView(wv);
+				alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.dismiss();
+					}
+				});
+				alert.show();
+			}
+		});
+		//portale gaia
+		((TextView)v.findViewById(R.id.about_portale_gaia)).setText(Html.fromHtml("<a href=\"\">Portale Gaia</a>"));
+		((TextView)v.findViewById(R.id.about_portale_gaia)).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Internet("https://gaia.cri.it");
+			}
+		});
+		//informazioni gaia
+		((TextView)v.findViewById(R.id.about_gaia)).setText(Html.fromHtml("<a href=\"\">Informazioni GAIA</a>"));
+		((TextView)v.findViewById(R.id.about_gaia)).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Internet("https://gaia.cri.it/?p=public.about");
+			}
+		});
+		//portale sviluppo mobile
+		((TextView)v.findViewById(R.id.about_gaia_mobile)).setText(Html.fromHtml("<a href=\"\">Codice Sorgente Gaia Mobile</a>"));
+		((TextView)v.findViewById(R.id.about_gaia_mobile)).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Internet("https://github.com/CroceRossaItaliana/gaia-android");
+			}
+		});
+		//infomrazione dati
+		((TextView)v.findViewById(R.id.about_gaia_privacy)).setText(Html.fromHtml("<a href=\"\">Informazioni Gestione Dati</a>"));
+		((TextView)v.findViewById(R.id.about_gaia_privacy)).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Internet("https://gaia.cri.it/?p=public.privacy");
+			}
+		});
+
+
+
+		//((WebView)v.findViewById(R.id.about_info))//.setClickable(true);;	
 		return v;
 	}	
-	
-	private void aggiornalist() {
 
-		if(info!=null){
-			//Questa è la lista che rappresenta la sorgente dei dati della listview
-			//ogni elemento è una mappa(chiave->valore)
-			ArrayList<HashMap<String, Object>> data=new ArrayList<HashMap<String,Object>>();
-
-			HashMap<String,Object> ServiceMap=new HashMap<String, Object>();//creiamo una mappa di valori
-			for(int i=0;i<info.size();i++){
-				ServiceMap=new HashMap<String, Object>();//creiamo una mappa di valori
-				ServiceMap.put("about_title", info.get(i));
-				ServiceMap.put("about_url",url.get(i));
-				data.add(ServiceMap);  //aggiungiamo la mappa di valori alla sorgente dati
-			}
-			String[] from={"about_title","about_url"}; //dai valori contenuti in queste chiavi
-			int[] to={R.id.about_title,R.id.about_url};//agli id delle view
-
-			//costruzione dell adapter
-			SimpleAdapter adapter=new SimpleAdapter(
-					context,
-					data,//sorgente dati
-					R.layout.riga_about, //layout contenente gli id di "to"
-					from,
-					to);
-			//utilizzo* dell'adapter
-			listview.setAdapter(adapter);
-		}else{
-			ArrayAdapter<String> arrayAdapter =new ArrayAdapter<String>(context, R.layout.riga_about, R.id.textViewList,new String[]{"Caricamento.."});
-			listview.setAdapter(arrayAdapter);
-		}
+	private void Internet(String url){
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		startActivity(i);
 	}
-	
-	
-	
 }
